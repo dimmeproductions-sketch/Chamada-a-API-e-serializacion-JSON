8e33ee61675972b8626d7b28a7aaa820aec7002b
Daniel <dimmeproductions@gmail.com>
2026-04-22 21:28:05 +0200

    Añadida la clase para contener la lista de preguntas

---
+++ Assets/Scripts/RespuestaTrivia.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class RespuestaTrivia
+{
+    public int codigo_respuesta;
+    public List<Pregunta> resultados;
+}
